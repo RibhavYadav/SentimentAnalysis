4bcc3e49060ff6d2040d1f11fae84794d9bd9820
draby2466 <ribhavyadav2466@gmail.com>
2025-01-31 00:21:04 +0530

    Basic function to transfer text.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,14 @@
 <script lang="ts">
+	let text;
+
+	async function getText() {
+		const response = await fetch('http://127.0.0.1:8000/analyze/', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ text })
+		});
+		const data = await response.json();
+	}
 </script>
 
 <div class="flex justify-center bg-[#1e1f22] text-white">
@@ -10,10 +20,12 @@
 <div class="flex w-full flex-col items-center gap-2 p-3">
 	<div class="w-5/6 gap-3 p-3">
 		<p class="mb-1.5">Enter text here:</p>
-		<textarea class="h-48 w-full rounded-lg bg-blue-300 p-2 shadow-md shadow-blue-600/50"
+		<textarea
+			bind:value={text}
+			class="h-48 w-full rounded-lg bg-blue-300 p-2 shadow-md shadow-blue-600/50"
 		></textarea>
 		<div class="mt-2 flex w-full justify-end">
-			<button class="btn">Analyze </button>
+			<button class="btn" on:click={getText}>Analyze</button>
 		</div>
 	</div>
 </div>
