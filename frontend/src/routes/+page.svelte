d9a068785a0e2f3579ebe08ad273b7050c7602bd
draby2466 <ribhavyadav2466@gmail.com>
2025-01-30 23:06:06 +0530

    Base page structure to get text.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,19 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div class="flex justify-center bg-[#1e1f22] text-white">
+	<div class="m-1 flex h-20 w-2/4 items-center justify-center p-3 text-3xl font-bold">
+		Sentiment Analysis
+	</div>
+</div>
+
+<div class="flex w-full flex-col items-center gap-2 p-3">
+	<div class="w-5/6 gap-3 p-3">
+		<p class="mb-1.5">Enter text here:</p>
+		<textarea class="h-48 w-full rounded-lg bg-blue-300 p-2 shadow-md shadow-blue-600/50"
+		></textarea>
+		<div class="mt-2 flex w-full justify-end">
+			<button class="btn">Analyze </button>
+		</div>
+	</div>
+</div>
