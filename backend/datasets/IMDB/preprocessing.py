5ff80529e5697408d56ccbc44184a0fb7a719fa0
draby2466 <ribhavyadav2466@gmail.com>
2025-02-10 23:11:56 +0530

    Added IMDB dataset along with the preprocessing.py file.

--- backend/datasets/IMDB/preprocessing.py
+++ backend/datasets/IMDB/preprocessing.py
@@ -1,40 +1,10 @@
 import pandas as pd
-import numpy as np
-from collections import Counter
-from nltk.tokenize import word_tokenize
 
-# Load data and tokenize text
+# Load dataset
 df = pd.read_csv('IMDB-Dataset.csv')
-df["tokens"] = df["review"].apply(word_tokenize)
 
-# Assign values to the sentiment
-df["sentiment"] = df["sentiment"].map({"negative": 0, "positive": 1})
+# Map sentiment Negative -> 0, Positive -> 1
+df["sentiment"] = df["sentiment"].map({'positive': 1, 'negative': 0})
 
-# Build vocabulary
-counter = Counter()
-for token in df["tokens"]:
-    counter.update(token)
-
-# Assign indices to most common words
-MAX_VOCAB_SIZE = 50000
-vocab = {"<PAD>": 0, "<UNK>": 1}
-for word, _ in counter.most_common(MAX_VOCAB_SIZE - len(vocab)):
-    vocab[word] = len(vocab)
-
-np.save("vocab.npy", vocab)
-
-# Convert tokens to input IDs using vocab
-df["input_ids"] = df["tokens"].apply(lambda tokens: [vocab.get(token, vocab["<UNK>"]) for token in tokens])
-
-
-def text_pipline(tokens):
-    return [vocab.get(token, vocab["<UNK>"]) for token in tokens]
-
-
-# Indexed tokens for the reviews
-df["input_ids"] = df["tokens"].apply(text_pipline)
-df.drop("review", axis="columns", inplace=True)
-df.drop("tokens", axis="columns", inplace=True)
-
-# Save processed data
-df.to_json("IMDB-Processed.json", orient="records", lines=True)
+# Save processed file
+df.to_csv('IMDB-Dataset_processed.csv', index=False)
