7acac7303de74d243a36c855a6c247f157c18ab7
draby2466 <ribhavyadav2466@gmail.com>
2025-02-11 00:56:37 +0530

    Optimized data_model.py for faster data loading and transfer.

--- backend/datasets/data_model.py
+++ backend/datasets/data_model.py
@@ -1,8 +1,8 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
 
 
-# Dataset class
 class SentimentDataset(Dataset):
     def __init__(self, text, labels, vocab):
         self.text = text
@@ -13,13 +13,15 @@ class SentimentDataset(Dataset):
         return len(self.labels)
 
     def __getitem__(self, idx):
-        return self.text[idx], self.labels[idx]
+        text = torch.tensor(self.text[idx], dtype=torch.long)
+        label = self.labels[idx]
+        return text, label
 
     def collate_fn(self, batch):
         text, labels = zip(*batch)
-        max_len = max(len(t) for t in text)
-        padded_texts = [t + [self.vocab["<PAD>"]] * (max_len - len(t)) for t in text]
-        return torch.tensor(padded_texts, dtype=torch.long), torch.tensor(labels, dtype=torch.float32)
+        text = [torch.tensor(t, dtype=torch.long).clone().detach() for t in text]
+        padded_texts = pad_sequence(text, batch_first=True, padding_value=self.vocab["<PAD>"])
+        return padded_texts, torch.tensor(labels, dtype=torch.float32)
 
     def get_dataloader(self, batch_size=32, shuffle=True):
-        return DataLoader(self, batch_size, shuffle, collate_fn=self.collate_fn)
+        return DataLoader(self, batch_size, shuffle, collate_fn=self.collate_fn, pin_memory=True)
