dc06a12e2c9b44b8eda4001f175a12d1ea01c84f
draby2466 <ribhavyadav2466@gmail.com>
2025-02-10 23:39:38 +0530

    Update preprocessing.py to ensure processed datasets have same column names along with their order.

--- backend/datasets/IMDB/preprocessing.py
+++ backend/datasets/IMDB/preprocessing.py
@@ -3,6 +3,9 @@ import pandas as pd
 # Load dataset
 df = pd.read_csv('IMDB-Dataset.csv')
 
+# Rename columns
+df = df.rename(columns={"review": "text", "sentiment": "sentiment"})
+
 # Map sentiment Negative -> 0, Positive -> 1
 df["sentiment"] = df["sentiment"].map({'positive': 1, 'negative': 0})
 
--- backend/datasets/Tweets/preprocessing.py
+++ backend/datasets/Tweets/preprocessing.py
@@ -13,5 +13,8 @@ df.drop("user", axis="columns", inplace=True)
 # Map sentiment Negative: 0 -> 0, Positive: 4 -> 1
 df["sentiment"] = df["sentiment"].map({0: 0, 4: 1})
 
+# Rearrange columns
+df = df[["text", "sentiment"]]
+
 # Save
 df.to_csv("tweet_data_processed.csv", index=False)
