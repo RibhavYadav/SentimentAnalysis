6cc5810f6c2e82b2244fecd19ba35ba37fe10552
draby2466 <ribhavyadav2466@gmail.com>
2025-02-10 23:05:42 +0530

    Added tweets dataset along with the preprocessing.py file.

---
+++ backend/datasets/Tweets/preprocessing.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Load data and assign column names
+column_names = ["sentiment", "id", "date", "flag", "user", "text"]
+df = pd.read_csv("tweet_data.csv", names=column_names, header=None, encoding="latin1")
+
+# Drop ID, date, user and flag as it isn't required for sentiment analysis
+df.drop("id", axis="columns", inplace=True)
+df.drop("date", axis="columns", inplace=True)
+df.drop("flag", axis="columns", inplace=True)
+df.drop("user", axis="columns", inplace=True)
+
+# Map sentiment Negative: 0 -> 0, Positive: 4 -> 1
+df["sentiment"] = df["sentiment"].map({0: 0, 4: 1})
+
+# Save
+df.to_csv("tweet_data_processed.csv", index=False)
