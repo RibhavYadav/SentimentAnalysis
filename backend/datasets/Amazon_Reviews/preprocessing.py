d55114a299e290706ad34f6839408ef7f0c20194
draby2466 <ribhavyadav2466@gmail.com>
2025-02-12 18:17:26 +0530

    Changed order of columns, added processed file to .gitignore.

--- backend/datasets/Amazon_Reviews/preprocessing.py
+++ backend/datasets/Amazon_Reviews/preprocessing.py
@@ -3,13 +3,16 @@ import pandas as pd
 # Download dataset from here: https://www.kaggle.com/datasets/kritanjalijain/amazon-reviews/
 
 # Load dataset
-df = pd.read_csv('./train.csv', names=["sentiment", "review_title", "review"])
+df = pd.read_csv('amazon_reviews.csv', names=["sentiment", "review_title", "review"])
 
 # Remove unnecessary column: review_title
 df.drop("review_title", axis="columns", inplace=True)
 
 # Map sentiment values: Negative: 1 -> 0, Positive: 2 -> 1
 df["sentiment"] = df["sentiment"].map({1: 0, 2: 1})
+
+# Change order of columns for consistency
+df = df[["review", "sentiment"]]
 
 # Save processed data
 df.to_csv("amazon_reviews_processed.csv", index=False)
