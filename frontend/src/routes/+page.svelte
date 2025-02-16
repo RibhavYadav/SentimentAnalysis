ec83c299a3e64a661898d2e1bd9fbb1739fdd94b
draby2466 <ribhavyadav2466@gmail.com>
2025-02-17 01:01:30 +0530

    Added template for output visualization.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,11 @@
-<script lang="ts">
-	let text;
+<script lang="ts" xmlns="http://www.w3.org/1999/html">
+	let text: string;
+	let test = [
+		{ word: 'happy', value: 0.8 },
+		{ word: 'sad', value: -0.6 },
+		{ word: 'neutral', value: 0.1 }
+	];
+	let predictions: string;
 
 	async function getText() {
 		const response = await fetch('http://127.0.0.1:8000/analyze/', {
@@ -7,7 +13,7 @@
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({ text })
 		});
-		const data = await response.json();
+		predictions = await response.json();
 	}
 </script>
 
@@ -17,15 +23,45 @@
 	</div>
 </div>
 
-<div class="flex w-full flex-col items-center gap-2 p-3">
-	<div class="w-5/6 gap-3 p-3">
+<div class="flex w-full flex-col items-center gap-2 p-2">
+	<div class="w-5/6 gap-3">
 		<p class="mb-1.5">Enter text here:</p>
 		<textarea
 			bind:value={text}
 			class="h-48 w-full rounded-lg bg-blue-300 p-2 shadow-md shadow-blue-600/50"
 		></textarea>
-		<div class="mt-2 flex w-full justify-end">
+		<div class="mt-2 flex w-full justify-end p-1">
 			<button class="btn" on:click={getText}>Analyze</button>
+		</div>
+	</div>
+</div>
+
+<div class="flex w-full flex-col items-center gap-2 p-2">
+	<div class="w-5/6 gap-3">
+		<div class="mb-1.5">Model prediction: <strong>{predictions}</strong></div>
+		<div
+			class="flex min-h-24 w-full flex-wrap gap-3 rounded-lg bg-blue-300 p-2 shadow-md shadow-blue-600/50"
+		>
+			{#each test as p}
+				<div class="flex w-full justify-around gap-2 p-1">
+					<div
+						class="max-w-56"
+						class:positive={p.value >= 0.8}
+						class:neutral={p.value > 0.4 && p.value < 0.8}
+						class:negative={p.value < 0.4}
+					>
+						{p.word}
+					</div>
+					<div
+						class="max-w-56"
+						class:positive={p.value >= 0.8}
+						class:neutral={p.value > 0.4 && p.value < 0.8}
+						class:negative={p.value < 0.4}
+					>
+						{p.value}
+					</div>
+				</div>
+			{/each}
 		</div>
 	</div>
 </div>
